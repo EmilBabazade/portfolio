bbd13c8c1961a772f53d7bbd42252f7be4749d98
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2023-01-12 14:11:09 +0200

    language files

---
+++ src/assets/languages/azeri.js
@@ -0,0 +1,54 @@
+export default {
+  app: {
+    tab1: "Ümumi",
+    tab2: "Təcrübə",
+    tab3: "Təhsil və Sertifikatlar",
+    tab4: "Əlaqə",
+  },
+  contentBox: {
+    untilNow: "Hal Hazıradək",
+    roles: "Vəzifələr",
+  },
+  contact: {
+    heading: "Əlaqə",
+  },
+  home: {
+    heading: "Salam, Mən İslam Babayevəm.",
+    details: [
+      `25 il iş təcrübəsi olan bir mühasibəm. Bank, xidmət, rabitə, tikinti, satış, neft sektorlarında uzunmüddətli iş
+      təcrübəm var. İstehsal, satış və digər sahələrdə konsaltinq xidmətləri göstərirəm. Müəssisələrdə Azərbaycan
+      Respublikası qanunvericiliyinə uyğun olaraq vergi uçotunun və Beynəlxalq Mühasibat Uçotu Standartlarına uyğun
+      olaraq maliyyə uçotunun qurulmasında və aparılmasında kifayət qədər təcrübəm var. Hal-hazırda neft hasilatı ilə
+      məşgul olan bir şirkətdə maliyyə meneceri işləyirəm.`,
+      `Mühasibat uçotunun tədrisi sahəsində kifayət qədər təcrübəm var. Bu sahədə bir neçə layihədə iştirak etmişəm və
+      hal-hazırda bu işlə fərdi məşğulam. (Azərbaycan dilində F3 layihəsi, ABTC tərəfindən tədris olunan “Ümumi bank
+      işi” tədris layihəsinə daxil olan “Bank işinin komputerdə simulyasiyası” layihəsi, yeni başlayanlar üçün mühasibat
+      uçotunun tədrisi və s.) Azərbaycan Bank Tədris Mərkəzinin ekspertiyəm.`,
+      `Fliranser kimi mühasibat uçotunun autsorsinq prinsipi ilə aparılması üzrə fəaliyyət göstərirəm. (VÖEN 2001280922).
+      “Creative Consultinq” konsaltinq şirkəti ilə əməkdaşlıq edirəm.`,
+      `Müəssisələrdə maliyyə proqnozlaşdırılması üzrə komandada iş təcrübəm var. İllik və aylıq gəlir-xərc büdcələrinin
+      proqnozlaşdırılması, balans proqnozlaşdırılması, pul vəsaitlərinin hərəkəti( birbaşa və dolayı metodlarla)
+      hesabatlarının proqnozlaşdırılması, il ərzində büdcənin yerinə yetirilməsinə nəzarət, təhlil və büdcəyə müvafiq
+      korreksiyaların edilməsinin praktikada tətbiqi ilə bağlı təcrübəyə malikəm.`,
+    ],
+    skillsHeading: "Əsas Bacarıqlar",
+    skills: [
+      "MS Word",
+      "MS Excel",
+      "MS Power Point",
+      "MS Outlook",
+      "Günəş maliyyə proqramı",
+      "Maliyyə menecmenti",
+      "Beynalaxalq Maliyyə",
+      "Hesabatları Standardları"
+    ],
+    languagesHeading: "Dil Bacarıqları",
+    languages: [
+      "Azərbaycan dili",
+      "Rus dili",
+      "Türk dili",
+      "Ingilis dili"
+    ],
+    cvButton: "CV"
+  }
+}
